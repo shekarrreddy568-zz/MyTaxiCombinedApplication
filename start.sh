6cc83cbe7c64b7d19707fcc78b7404798eca6e61
Rajashekar <Rajashekar.Chinthalapelly.extern@itergo.com>
2019-07-23 11:31:11 +0200

    new

--- start.sh
+++ start.sh
@@ -9,5 +9,5 @@ while ! nc -z ${KAFKA_HOST} ${KAFKA_PORT}; do
   sleep 0.1
 done
 
-java -jar ./paymentservice/target/scala-2.12/producer.jar &
-java -jar ./MyTaxiPaymentsConsumer/target/scala-2.12/consumer.jar
+java -jar ./paymentservice/target/scala-2.12/producer.jar >> /var/log/producer.log 2>&1 &
+java -jar ./MyTaxiPaymentsConsumer/target/scala-2.12/consumer.jar >> /var/log/consumer.log 2>&1
